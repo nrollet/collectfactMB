e98786a7de210552f1c94bca15ef0be2fae7ca4d
Nicolas ROLLET <nicolas@axe.lan>
2019-03-17 07:26:38 +0100

    go

--- collectefactMB/collectfactMB.py
+++ collectefactMB/collectfactMB.py
@@ -1,11 +1,10 @@
 import logging
 import json
 import pprint
-from openpyxl import load_workbook
+import openpyxl
 from fetchemail import FetchEmail
 from htmlparser import extract_htmltable
-import datetime
-
+from datetime import datetime
 
 
 logging.basicConfig(
@@ -18,6 +17,7 @@ def myconverter(o):
     if isinstance(o, datetime.datetime):
         return o.__str__()
 
+
 with open("config.json", "r") as f:
     config = json.load(f)
 
@@ -25,6 +25,16 @@ GIS_SRV = config["GLOBAL"]["GIS"]
 IMAP = config["GLOBAL"]["IMAP"]
 SENDER = config["GLOBAL"]["SENDER"]
 SUBJECT = config["GLOBAL"]["SUBJECT"]
+XL = config["GLOBAL"]["XLLOGGER"]
+
+### EXCEL LOGGER ####
+try:
+    wb = openpyxl.load_workbook(filename=XL)
+except OSError as e:
+    logging.error("{}".format(e))
+    wb = openpyxl.Workbook()
+ws = wb.active
+
 
 table_list = []
 for account in config["ACCOUNTS"].keys():
@@ -44,19 +54,28 @@ for account in config["ACCOUNTS"].keys():
         table = extract_htmltable(msg["body"])
         table_list.append(table)
 
+        for customer in table.keys():
+            for invoice in table[customer]:
+                ws.append(
+                    [
+                        datetime.now(),
+                        customer,
+                        invoice,
+                        table[customer][invoice]["montant"],
+                    ]
+                )
+
         # parsed = json.loads(table)
         # txt = json.dumps(table, indent=2, sort_keys=True, default=myconverter)
         # with open(account + "_" + msg["num"].decode() + ".json", "w") as f:
         #     f.write(txt)
-# txt = json.dumps(table_list, indent=2, sort_keys=True, default=myconverter)        
-# with open("toutjson", "w") as f:
-#     f.write(txt)
-#     print("x-"*20)
+    # txt = json.dumps(table_list, indent=2, sort_keys=True, default=myconverter)
+    # with open("toutjson", "w") as f:
+    #     f.write(txt)
+    #     print("x-"*20)
     mailsrv.close_connection()
 
-
-
-
-
+wb.save(XL)
+wb.close()
 
 # MAILSRV = origin["SERVER"]
